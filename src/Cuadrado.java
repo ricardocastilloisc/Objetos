c6283423221702fff2c91c88922c3330dcf462f0
ricardocastilloisc <ricardocastilloisc@gmail.com>
2018-10-03 21:03:52 -0500

    terminanado la clase cuadrado
    todos los calculos del cuadrado

--- src/Cuadrado.java
+++ src/Cuadrado.java
@@ -1,8 +1,31 @@
+import org.omg.CORBA.PRIVATE_MEMBER;
 
 public class Cuadrado {
-
-	public Cuadrado() {
+	//atributo
+	private double lado;
+	//constructor
+	public Cuadrado(double parametrolado) {
 		// TODO Auto-generated constructor stub
+		
+		this.lado = parametrolado;
+	}
+	//acceder a lado
+	public double getLado() {
+		return lado;
+	}
+	//modificar lado
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+	//calcular area
+	public double Area() 
+	{
+		//retorna ya con el calculo
+		return Math.pow(this.lado, 2.0);
+	}
+	public double Perimetro() 
+	{
+		return this.lado*4;
 	}
 
 }
